cc77a0c2827e4e95bbcfa28a84f94c94c86b2e88
akayado <g.d.taku.0902@gmail.com>
2015-09-10 17:22:52 +0900

    added Japanese version.

--- ja.php
+++ ja.php
@@ -19,22 +19,22 @@ require_once "out.php";
 	</head>
 	<body>
 		<div class="wrapper">
-			<h1><a href="http://www.akayado.com/linkchecker/">Link Checker</a></h1>	
+			<h1><a href="http://www.akayado.com/linkchecker/">Link Checker</a></h1>
 			<p class="align-center">
-				ENGLISH | <a href="ja.php">日本語</a>
+				<a href="index.php">ENGLISH</a> | 日本語
 			</p>
 
 <?php if(!$already_in): ?>
 			<form id="lcform" class="small-wrapper">
 				<div class="inputgroup">
-					<header><h2>URL to check</h2></header>
+					<header><h2>チェックするURL</h2></header>
 					<main>
 						<input type="text" id="start-url" placeholder="http://..." />
 					</main>
 				</div>
 
 				<div id="tags-to-check" class="inputgroup clearboth">
-					<header><h2>Tags to check</h2></header>
+					<header><h2>チェックするタグ</h2></header>
 					<main>
 						<label><input type="checkbox" id="chk-a" checked />&lt;a&gt;</label>
 						<label><input type="checkbox" id="chk-img" />&lt;img&gt;</label>
@@ -47,23 +47,23 @@ require_once "out.php";
 						<label><input type="checkbox" id="chk-audio" />&lt;audio&gt;</label>
 					</main>
 					<div class="align-center">
-						<button type="button" id="chk-all">Check All</button>
-						<button type="button" id="unchk-all">Uncheck All</button>
+						<button type="button" id="chk-all">全て選択</button>
+						<button type="button" id="unchk-all">全て非選択</button>
 					</div>
 				</div>
 
 				<div id="advanced" class="inputgroup">
-					<header><h2>Advanced</h2></header>
+					<header><h2>高度な設定</h2></header>
 					<main>
-						<label><input type="checkbox" id="chk-rec" checked />Check recursively</label><br />
-						<label>Maximum level of recursion: <input type="number" id="rec-num" value="2" min="1" max="5" /></label><br />
-						<p class="explanation">Links will be checked recursively. Maximum recursion level 1 would mean only checking the given URL(i.e. no recursion).</p>
-						<p class="notice">Warning: Without domain restriction, the check may take extremely long time for large numbers of recursion. </p>
+						<label><input type="checkbox" id="chk-rec" checked />再帰的にチェックする</label><br />
+						<label>最大深度: <input type="number" id="rec-num" value="2" min="1" max="5" /></label><br />
+						<p class="explanation">リンク先のリンク先も再帰的にチェックします。 最大深度1の場合は指定したURLのページ内のリンクだけのチェックとなります。</p>
+						<p class="notice">注意: 対象ドメインの制限（下で設定）無しに再帰の深度を深くすると非常に長い時間がかかります。 </p>
 						<hr />
-						<label><input type="checkbox" id="chk-only-domain" checked />Recurse only within the domain</label>
-						<p class="explanation">Will check links TO other domains, but would not recurse further. Usually recommended.</p>
+						<label><input type="checkbox" id="chk-only-domain" checked />同一ドメイン内のページのみチェックする</label>
+						<p class="explanation">他ドメインへのリンクはチェックされますが、それより先はチェックしません。通常推奨されます。</p>
 						<hr />
-						<label><input type="checkbox" id="chk-report-redirects" />Report redirects as broken links</label>
+						<label><input type="checkbox" id="chk-report-redirects" />リダイレクトもリンク切れとして報告する</label>
 					</main>
 					<div class="align-right">
 						<a href="javascript:void(0);" id="show-adv">Show</a>
